39222a32c6a77a2643e5396635b6a38052e0bd05
Dicky Nih Boss <dickyassidiq45@gmail.com>
2023-06-14 10:14:24 +0700

    json

--- js/script.js
+++ js/script.js
@@ -11,3 +11,25 @@ document.addEventListener("click", function (e) {
     navbarmid.classList.remove("active");
   }
 });
+
+const productContainer = document.querySelector(".row-product ul");
+
+fetch("json/produk.json")
+  .then((Response) => Response.json())
+  .then((data) => {
+    let contentHTML = "";
+    data.produk.forEach((produk) => {
+      contentHTML += `
+    <div class= "card">
+    <img src"${produk.image}" alt="${produk.name}" style="width: 100%" />
+    <h1>${produk.name}</h1>
+    <h5>${produk.deskripsi}</h5>
+    <i data-feather="menu"></i>
+    </div>
+    `;
+    });
+    productContainer.innerHTML = contentHTML;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
